46a77ee155460f5663e03778db5dea01e1b6907b
Ferchke17 <ferdavs@naver.com>
2023-11-19 23:02:17 +0900

    first

--- lib/main.dart
+++ lib/main.dart
@@ -12,60 +12,101 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String buttomName = 'Click';
-  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('App title'),
-        ),
-        body: Center(
-            child: SizedBox(
-              width: double.infinity,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-              Column(
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        buttomName = 'Clicked';
-                      });
-                    },
-                    child: Text(buttomName),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        buttomName = 'Clicked';
-                      });
-                    },
-                    child: Text(buttomName),
-                  ),
-                ],
-              ),
-                      ],
-                    ),
-            )),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
-            BottomNavigationBarItem(
-                label: 'Settings', icon: Icon(Icons.settings))
-          ],
-          currentIndex: currentIndex,
-          onTap: (int index) {
-            setState(() {
-              currentIndex = index;
-            });
-          },
-        ),
+        debugShowCheckedModeBanner: false, home: MyAppExtension());
+  }
+}
+
+class MyAppExtension extends StatefulWidget {
+  const MyAppExtension({super.key});
+
+  @override
+  State<MyAppExtension> createState() => _MyAppExtensionState();
+}
+
+class _MyAppExtensionState extends State<MyAppExtension> {
+  String buttomName = ':)';
+  int currentIndex = 0;
+  int myRecord = 0;
+  bool _isClicked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('App title'),
       ),
+      body: Center(
+        child: currentIndex == 0
+            ? Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: Colors.red,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.red,
+                          backgroundColor: Colors.orange),
+                      onPressed: () {
+                        setState(() {
+                          buttomName = '(:';
+                        });
+                      },
+                      child: Text(buttomName),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (BuildContext buildContext) =>
+                                const NextPage(),
+                          ),
+                        );
+                      },
+                      child: const Text('My Record'),
+                    ),
+                  ],
+                ),
+              )
+            : GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _isClicked = !_isClicked;
+                  });
+                },
+                child: _isClicked
+                    ? Image.asset('images/IMGS.png')
+                    : Image.network(
+                        "https://blog.kakaocdn.net/dn/0mySg/btqCUccOGVk/nQ68nZiNKoIEGNJkooELF1/img.jpg"),
+              ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
+          BottomNavigationBarItem(label: 'Game', icon: Icon(Icons.gamepad))
+        ],
+        currentIndex: currentIndex,
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class NextPage extends StatelessWidget {
+  const NextPage({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
     );
   }
 }
